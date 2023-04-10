b950c1524653e18b143dbe7923ede920e58a068a
RRoxxxsii <mishabur38@yandex.com>
2023-04-10 16:25:42 +0300

    models: started new account application, redefined user model also created model for categories and posts testing: installed django-pytest, ran one simple test to check if it works correctly, added a file pytest.ini to make a simple configuration for the tests

---
+++ forum/account/models.py
@@ -0,0 +1,68 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.core.mail import send_mail
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomAccountManager(BaseUserManager):
+
+    def create_superuser(self, email, name, password, **kwargs):
+
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        kwargs.setdefault('is_active', True)
+
+        if kwargs.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(email, name, password, **kwargs)
+
+    def create_user(self, email, name, password, **other_fields):
+
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,
+                          **other_fields)
+        user.set_password(password)
+        user.save()
+        return
+
+
+class Author(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    user_name = models.CharField(max_length=150)
+    mobile = models.CharField(max_length=20, blank=True)
+
+    # User Status
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    class Meta:
+        verbose_name = "Accounts"
+        verbose_name_plural = "Accounts"
+
+    def __str__(self):
+        return self.user_name
+
+    def email_user(self, subject, message):
+        send_mail(
+            subject,
+            message,
+            'l@1.com',
+            [self.email],
+            fail_silently=False,
+        )
